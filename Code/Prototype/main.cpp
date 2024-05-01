d758cc627cf93ab6b96d1cf1542d04d2c63c43da
Kemar Wilson <kem.wilson@ku.edu>
2024-04-30 21:27:30 -0500

    Bug fixes

--- Code/Prototype/main.cpp
+++ Code/Prototype/main.cpp
@@ -9,12 +9,13 @@ int main()
     // Main logic for the menu options
     int choice;
     int i = 1;
-    int h = 1;
     std::string expression;
     std::unordered_map<char, bool> variables;
 
     while (i)
     {
+        int h = 1; // Reset h to 1 at the beginning of each iteration
+
         // Menu options logic
         std::cout << "\033[1;31m\nMenu Options:\033[0m" << std::endl;
         std::cout << "\033[1;32m1. Boolean Logic Calculator\033[0m" << std::endl;
@@ -24,9 +25,9 @@ int main()
         std::cout << "\033[1;31mEnter your choice: \033[0m";
 
         // Input verification for integer input
-        while (!(std::cin >> choice))
+        while (!(std::cin >> choice) || choice < 1 || choice > 3)
         {
-            std::cout << "Invalid input. Please enter an integer." << std::endl;
+            std::cout << "Invalid input. Please input a valid integer between 1 and 3, inclusive." << std::endl;
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
@@ -43,16 +44,18 @@ int main()
                 std::cout << "\033[1;32m\nBoolean Logic Calculator\n\033[0m" << std::endl;
                 std::cout << "\033[1;31mEnter a Boolean expression or X to go back: \033[0m";
                 std::getline(std::cin, expression);
-
-                if (expression != "X" && expression != "x")
+                
+                if (expression == "X" || expression == "x")
                 {
+                    break; // Exit the inner while loop
+                }
+
+                try {
                     bool result = evaluateExpression(expression, variables);
                     std::cout << "Expression: " << expression << std::endl;
                     std::cout << "Result: " << (result ? "True" : "False") << std::endl;
-                }
-                else
-                {
-                    h = 0;
+                } catch (const std::exception& e) {
+                    std::cerr << "Error: " << e.what() << std::endl;
                 }
             }
             break;
